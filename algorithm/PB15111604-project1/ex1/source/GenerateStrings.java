2517d5bda810ece0a81d8f66d089a7b596809342
PB15111604 <jzw0222@mail.ustc.edu.cn>
2017-10-13 20:43:12 +0800

    input data generated.

---
+++ algorithm/PB15111604-project1/ex1/source/GenerateStrings.java
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: GenerateStrings.java
+	> Author: King.Zevin
+	> Mail: jzw0222@mail.ustc.edu.cn
+	> Created Time: Fri Oct 13 18:28:01 2017
+ ************************************************************************/
+
+import java.io.*;
+import java.util.*;
+
+public class GenerateStrings{
+    public static void main(String[] args) throws IOException{
+        PrintWriter outString = new PrintWriter("./input_strings.txt", "UTF-8");
+        for(int i = 0; i < (1 << 17) ; i++){
+            int l = (int)(Math.random() * 32 + 1);
+            String s = "";
+            for(int j = 0; j < l; j++){
+                int k = (int)(Math.random() * 26);
+                char c = (char)('a' + k);
+                s += c;
+            }
+            outString.println(s);
+            outString.flush();
+        } 
+    }
+}
+
---
+++ algorithm/PB15111604-project1/ex2/source/GenerateInteger.java
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: GenerateInteger.java
+	> Author: King.Zevin
+	> Mail: jzw0222@mail.ustc.edu.cn
+	> Created Time: Fri Oct 13 18:28:01 2017
+ ************************************************************************/
+
+import java.io.*;
+import java.util.*;
+
+public class GenerateInteger{
+    public static void main(String[] args) throws IOException{
+        PrintWriter outInt = new PrintWriter("./input_integers.txt", "UTF-8");
+        for(int i = 0; i < (1 << 17) ; i++){
+            int r = (int)(Math.random() * 65535 + 1);
+            outInt.println(r);
+            outInt.flush();
+        } 
+    }
+}
+
